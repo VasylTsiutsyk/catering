39cf30d5efd51997b382316f25a316bcfaf60000
Vasyl Tsiutsyk <v.tsyutsyk@gmail.com>
2020-11-02 17:03:58 +0200

    init

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const mySwiper = new Swiper('.swiper-container', {
+
+    direction: 'horizontal',
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 20,
+    slidesPerView: 2,
+    centeredSlides: true,
+    speed: 500,
+
+
+    pagination: {
+        el: '.swiper-pagination',
+    }
+})
+
+//scrollbar
+window.addEventListener('scroll', scrollFunction);
+
+function scrollFunction() {
+    const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    const scrolled = (winScroll / height) * 100;
+    document.querySelector("#progressBar").style.width = scrolled + "%";
+}
